5ad30184d3493a2330942f1fc13d7d75c9d90177
Niloy Quazi <niloy940@gmail.com>
2019-11-09 02:27:11 +0600

    Product Resource created

---
+++ eapi/app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->details,
+            'price' => $this->price,
+            'stock' => $this->stock == 0 ? 'Out of stock!' : $this->stock,
+            'discount' => $this->discount,
+
+            'total_price' => round((($this->discount/100) * $this->price), 2),
+
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(), 2) : 'No Rating Yet!',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
+        ];
+    }
+}
--- eapi/routes/api.php
+++ eapi/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 
 /*
